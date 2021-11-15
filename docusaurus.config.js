648bdbb5cde9424f4f3ad318bbc5262b9874f0bf
Christopher Phan <cpp6d@mail.missouri.edu>
2021-11-14 20:31:54 -0800

    fix: updated home page error

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -101,7 +101,6 @@ module.exports = {
       {
         docs: {
           // It is recommended to set document id as docs home page (`docs/` path).
-          homePageId: 'basics',
           sidebarPath: require.resolve('./sidebars.js'),
           // Please change this to your repo.
           editUrl:
